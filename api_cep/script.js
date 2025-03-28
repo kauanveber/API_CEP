7c1dc3b0b9f33e71aca31b9f5cffc009204996d1
KAUAN SCHUHARDT DA SILVA VEBER <kauan.veber@alunos.sc.senac.br>
2025-03-28 14:33:20 -0300

    acrescentando a validação da busca

--- api_cep/script.js
+++ api_cep/script.js
@@ -2,15 +2,33 @@
 function loadLogradouro(cep) {
     fetch(`http://viacep.com.br/ws/${cep}/json`)
         .then(data => data.json())
-        .then(dados => buscarDados(dados))
+        .then(dados => {
+            if (dados.erro) {
+                erro("CEP não encontrado");
+            } else {
+                buscarDados(dados);
+            }
+        })
+        .catch(err => erro(err.message));
 }
 
 function buscarDados(dados) {
+    
+    
     document.getElementById("logradouro").value = dados.logradouro
     document.getElementById("bairro").value = dados.bairro
     document.getElementById("cidade").value = dados.localidade
     document.getElementById("uf").value = dados.uf
     document.getElementById("ibge").value = dados.ibge
+    
+}
+function erro(erro) {
+    alert("Erro: " + erro);
+    document.getElementById("logradouro").value = "";
+    document.getElementById("bairro").value = "";
+    document.getElementById("cidade").value = "";
+    document.getElementById("uf").value = "";
+    document.getElementById("ibge").value = "";
 }
 
 
